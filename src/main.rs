f83a93f3498a06f632bc253a7af75743519a6d1b
三咲智子 Kevin Deng <sxzz@sxzz.moe>
2024-07-09 23:31:04 +0800

    feat: error handing

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,109 @@
-use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-use std::sync::Arc;
-use std::{fs::File, io::BufReader};
-use tokio::io::{copy_bidirectional, AsyncRead, AsyncReadExt, AsyncWrite};
-use tokio::net::{TcpListener, TcpStream};
-use tokio_rustls::rustls::pki_types::{CertificateDer, PrivateKeyDer};
-use tokio_rustls::rustls::ServerConfig;
-use tokio_rustls::server::TlsStream;
-use tokio_rustls::TlsAcceptor;
+use std::{
+    fs::File,
+    io::{self, BufReader},
+    net::{IpAddr, Ipv4Addr, Ipv6Addr},
+    sync::Arc,
+};
+use tokio::{
+    io::{copy_bidirectional, AsyncRead, AsyncReadExt, AsyncWrite},
+    net::{TcpListener, TcpStream},
+};
+use tokio_rustls::{
+    rustls::{
+        pki_types::{CertificateDer, PrivateKeyDer},
+        ServerConfig,
+    },
+    TlsAcceptor,
+};
+use tracing::{error, info, level_filters::LevelFilter, span, Level};
 
 #[tokio::main]
-async fn main() {
-    let (certs, priv_key) = load_cert("pem/cert.pem", "pem/key.pem");
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::DEBUG)
+        .init();
+
+    let (certs, priv_key) = load_certs("pem/cert.pem", "pem/key.pem")?;
+
     let server_cfg = Arc::new(
         ServerConfig::builder()
             .with_no_client_auth()
-            .with_single_cert(certs, priv_key)
-            .unwrap(),
+            .with_single_cert(certs, priv_key)?,
     );
 
     let tls_acceptor = TlsAcceptor::from(server_cfg);
-    let tcp_listener = TcpListener::bind("0.0.0.0:443")
-        .await
-        .expect("failed to bind");
+
+    let address = "0.0.0.0:443";
+    let span = span!(Level::INFO, "TCP", "{}", address).entered();
+    info!("Starting TCP server...",);
+    let tcp_listener = TcpListener::bind(address).await.expect("failed to bind");
+    info!("Started TCP server.");
+    span.exit();
 
     loop {
-        let (stream, _) = tcp_listener.accept().await.unwrap();
-        let tls_stream = tls_acceptor.accept(stream).await.unwrap();
-
-        tokio::spawn(async move {
-            handle(tls_stream).await;
-        });
+        match tcp_listener.accept().await {
+            Ok((stream, ..)) => {
+                if let Ok(tls_stream) = tls_acceptor
+                    .accept(stream)
+                    .await
+                    .inspect_err(|error| error!("{error}"))
+                {
+                    tokio::spawn(async {
+                        if let Err(err) = handle(tls_stream).await {
+                            error!("{err}")
+                        };
+                    });
+                }
+            }
+            Err(err) => {
+                error!("{err}");
+            }
+        }
     }
 }
 
-fn load_cert(
+fn load_certs(
     certs: &str,
     priv_key: &str,
-) -> (Vec<CertificateDer<'static>>, PrivateKeyDer<'static>) {
-    let mut certs = BufReader::new(File::open(certs).unwrap());
-    let mut priv_key = BufReader::new(File::open(priv_key).unwrap());
+) -> Result<(Vec<CertificateDer<'static>>, PrivateKeyDer<'static>), io::Error> {
+    let mut certs = BufReader::new(File::open(certs)?);
+    let mut priv_key = BufReader::new(File::open(priv_key)?);
 
-    let certs = rustls_pemfile::certs(&mut certs)
-        .collect::<Result<Vec<_>, _>>()
-        .unwrap();
-    let priv_key = rustls_pemfile::private_key(&mut priv_key).unwrap().unwrap();
+    let certs = rustls_pemfile::certs(&mut certs).collect::<Result<Vec<_>, _>>()?;
+    let priv_key = rustls_pemfile::private_key(&mut priv_key)?.ok_or(io::Error::new(
+        io::ErrorKind::InvalidData,
+        "no private key found",
+    ))?;
 
-    (certs, priv_key)
+    Ok((certs, priv_key))
 }
 
-async fn handle<IO: AsyncRead + AsyncWrite + Unpin>(mut tls_stream: TlsStream<IO>) {
+async fn handle<IO: AsyncRead + AsyncWrite + Unpin>(mut tls_stream: IO) -> anyhow::Result<()> {
     let mut passwd: [u8; 56] = [0_u8; 56];
-    tls_stream.read_exact(&mut passwd).await.unwrap();
+    tls_stream.read_exact(&mut passwd).await?;
 
     read_crlf(&mut tls_stream).await;
-    let cmd = tls_stream.read_u8().await.unwrap();
+    let cmd = tls_stream.read_u8().await?;
     if cmd != 0x1 {
-        return;
+        return Ok(());
     }
-    let address_type = tls_stream.read_u8().await.unwrap();
+    let address_type = tls_stream.read_u8().await?;
     let address = match address_type {
         0x1 => {
             // ipv4
-            let buf = tls_stream.read_u32().await.unwrap();
+            let buf = tls_stream.read_u32().await?;
             IpAddr::V4(Ipv4Addr::from(buf)).to_string()
         }
         0x3 => {
-            let length = tls_stream.read_u8().await.unwrap();
+            // domain
+            let length = tls_stream.read_u8().await?;
             let mut buf = vec![0_u8; length as usize];
-            tls_stream.read_exact(&mut buf).await.unwrap();
+            tls_stream.read_exact(&mut buf).await?;
             String::from_utf8(buf).unwrap()
         }
         0x4 => {
             // ipv6
-            let buf = tls_stream.read_u128().await.unwrap();
+            let buf = tls_stream.read_u128().await?;
             IpAddr::V6(Ipv6Addr::from(buf)).to_string()
         }
         _ => {
@@ -80,18 +111,18 @@ async fn handle<IO: AsyncRead + AsyncWrite + Unpin>(mut tls_stream: TlsStream<IO
         }
     };
 
-    let port = tls_stream.read_u16().await.unwrap();
+    let port = tls_stream.read_u16().await?;
     read_crlf(&mut tls_stream).await;
 
-    let mut target_tcp_stream = TcpStream::connect(format!("{}:{}", address, port))
-        .await
-        .unwrap();
+    let mut target_tcp_stream = TcpStream::connect(format!("{}:{}", address, port)).await?;
 
-    copy_bidirectional(&mut target_tcp_stream, &mut tls_stream)
-        .await
-        .unwrap();
+    println!("{}:{} connected", address, port);
+
+    copy_bidirectional(&mut target_tcp_stream, &mut tls_stream).await?;
+
+    Ok(())
 }
 
-async fn read_crlf<IO: AsyncRead + AsyncWrite + Unpin>(mut tls_stream: IO) {
+async fn read_crlf<IO: AsyncRead + Unpin>(mut tls_stream: IO) {
     tls_stream.read_u16().await.unwrap();
 }
